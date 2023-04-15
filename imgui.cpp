d1ff0411cce3bf202d46334dbccd4fc005fa375b
Jules Fouchy <jules.fouchy@ntymail.com>
2023-04-15 14:56:03 +0200

    Fix missing color enum in GetStyleColorName

--- imgui.cpp
+++ imgui.cpp
@@ -3234,6 +3234,7 @@ const char* ImGui::GetStyleColorName(ImGuiCol idx)
     case ImGuiCol_NavWindowingHighlight: return "NavWindowingHighlight";
     case ImGuiCol_NavWindowingDimBg: return "NavWindowingDimBg";
     case ImGuiCol_ModalWindowDimBg: return "ModalWindowDimBg";
+    case ImGuiCol_SliderGrabHovered: return "SliderGrabHovered";
     }
     IM_ASSERT(0);
     return "Unknown";
